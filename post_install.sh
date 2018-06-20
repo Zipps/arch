609af363f65fa4414c1ab23c1d8dce775d20a5c0
Shea Granville <shea.granville@gmail.coml>
2018-06-20 15:52:19 +1000

    Added more pre install configuration

---
+++ post_install.sh
@@ -0,0 +1,27 @@
+SSID=""
+WIFI_PASSPHRASE=""
+WIFI_ADAPTER="wlp1s0"
+USERNAME=""
+USERPW=""
+
+# setup network manager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+nmcli d wifi connect $SSID password $WIFI_PASSPHRASE iface $WIFI_ADAPTER
+
+# get best mirrors
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+vim /etc/pacman.d/mirrorlist
+
+# create user
+useradd -m -g wheel -s /bin/bash $USERNAME
+echo -en "$USERPW\n$USERPW" | passwd $USERNAME
+
+# install packages
+packman -S gnome gnome-extra gdm
+
+# enable display manager
+systemctl enable gdm
+systemctl start gdm.service
--- pre_install.sh
+++ pre_install.sh
@@ -1,4 +1,5 @@
 # parameters
+BEFOREWIFI=true
 MYHOSTNAME="arch"
 SSID=""
 WIFI_PASSPHRASE=""
@@ -8,15 +9,15 @@ ROOT_PASSWORD=""
 DRIVE_PASSPHRASE=""
 BOOT_DRIVE="/dev/nvme0n1p1"
 ROOT_DRIVE="/dev/nvme0n1p4"
+WINDOWS_BOOT="/dev/nvme0n1p2"
 CRYPT_NAME="cryptroot"
-MAP_DRIVE="/dev/mapper/$CRYPT_NAME"
+ROOT_MAP="/dev/mapper/$CRYPT_NAME"
+
+MAP_DRIVE="/dev/mapper/arch-root"
+SWAP_DRIVE="/dev/mapper/arch-swap"
 
 # larger font
 setfont sun12x22
-
-# connect to internet
-wpa_supplicant -B -i $WIFI_ADAPTER -c <(wpa_passphrase "$SSID" "$WIFI_PASSPHRASE")
-dhclient $WIFI_ADAPTER
 
 # update system clock
 timedatectl set-ntp true
@@ -25,22 +26,24 @@ timedatectl set-ntp true
 echo -en "$DRIVE_PASSPHRASE" | cryptsetup -c aes-xts-plain -y -s 512 luksFormat "$ROOT_DRIVE"
 echo -en "$DRIVE_PASSPHRASE" | cryptsetup luksOpen "$ROOT_DRIVE" $CRYPT_NAME
 
-# format drive
+pvcreate /dev/mapper/$CRYPT_NAME
+vgcreate arch /dev/mapper/$CRYPT_NAME
+lvcreate -L +8G arch -n swap
+lvcreate -l +100%FREE arch -n root
+
+# Create filesystems on your encrypted partitions
+
+mkswap $SWAP_DRIVE
 mkfs.ext4 $MAP_DRIVE
 
 # mount filesystems
 mount $MAP_DRIVE /mnt
+swapon $SWAP_DRIVE
 mkdir /mnt/boot
 mount $BOOT_DRIVE /mnt/boot
 
-# get best mirrors
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-vim /etc/pacman.d/mirrorlist
-
 # bootstrapping
-pacstrap /mnt base base-devel grub efibootmgr os-prober git wpa_supplicant dhclient intel-ucode
+pacstrap /mnt base base-devel grub efibootmgr os-prober git intel-ucode networkmanager
 
 # fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -70,10 +73,30 @@ vim /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 # setup GRUB
+mkdir /mnt/windows
+mount $WINDOWS_BOOT /mnt/windows 
+grub-mkfont --output=/boot/grub/fonts/DejaVuSansMono20.pf2 \ --size=20 /usr/share/fonts/TTF/dejavu/DejaVuSansMono.ttf
+echo "menuentry \"System shutdown\" {
+	echo \"System shutting down...\"
+	halt
+}" >> /etc/grub.d/40_custom
+echo "menuentry \"System restart\" {
+	echo \"System rebooting...\"
+	reboot
+}" >> /etc/grub.d/40_custom
+fs_uuid=grub-probe --target=fs_uuid esp/EFI/Microsoft/Boot/bootmgfw.efi
+hints_string=grub-probe --target=hints_string esp/EFI/Microsoft/Boot/bootmgfw.efi
+echo "menuentry \"Microsoft Windows\" {
+		insmod part_gpt
+		insmod fat
+		insmod search_fs_uuid
+		insmod chain
+		search --fs-uuid --set=root $hints_string $fs_uuid
+		chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+}" >> /etc/grub.d/40_custom
 vim /etc/defaults/grub
 grub-install --target=x86_64-efi --recheck --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-
 
 # finish and reboot
 exit
